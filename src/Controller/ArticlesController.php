72232edb80fc1a247896368cc1ba45508d990eae
asketsystem <asketsystem1@gmail.com>
2021-12-23 11:55:44 +0000

    Add Entities

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -4,11 +4,39 @@ namespace App\Controller;
 
 class ArticlesController extends AppController
 {
+    public function initialize(): void
+    {
+        parent::initialize();
+
+        $this->loadComponent('Paginator');
+        $this->loadComponent('Flash');
+    }
+
     public function index() {
         $this->loadComponent('Paginator');
 
         $articles = $this->Paginator->paginate($this->Articles->find());
 
         $this->set('articles', $articles);
+    }
+
+    public function add(){
+        $article = $this->Articles->newEmptyEntity();
+
+        if ($this->request->is('post')) {
+            $article = $this->Articles->patchEntity($article, $this->request->getData());
+
+            $article->user_id = 1;
+
+            if ($this->Articles->save($article)) {
+                $this->Flash->success("Your article has been saved ;-)");
+                return $this->redirect(['action'=> 'index']);
+            }
+
+            // Couldn't save
+            $this->Flash->error('Unable to save article :-(');
+        }
+
+        $this->set('article', $article);
     }
 }
